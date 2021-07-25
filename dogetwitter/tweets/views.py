5f996df45d198c0ea9f7854a37e0d26ebeb1e0fc
liyahshaikh <liyahshaikh965@gmail.com>
2021-07-25 19:51:53 +0530

    Tweet view and template setup

--- dogetwitter/tweets/views.py
+++ dogetwitter/tweets/views.py
@@ -1,6 +1,39 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404,JsonResponse
 from django.shortcuts import render
-
+from .models import Tweet
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>What color is a sunflower?<br>Yellllloow!</h1>")
+    # return HttpResponse("<h1>What color is a sunflower?<br>Yellllloow!</h1>")
+    return render(request, "pages/home.html", context={}, status=200 )
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by Javascript/java/swift/iOS/Android
+    """
+    queryList = Tweet.objects.all()
+    tweet_list = [{'id': tweet.id, 'content': tweet.content} for tweet in queryList]
+    data={
+        'response':tweet_list
+    }
+    return JsonResponse(data)
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by Javascript/java/swift/iOS/Android
+    """
+    data={
+        "id":tweet_id,
+        # "content": obj.content,
+        # #image here later once we figure out how to store it :)
+        
+    }
+    status=200
+    try:
+        obj=Tweet.objects.get(id=tweet_id)
+        data['content']=obj.content
+    except:
+        data['message']="Not Found"
+        status=404
+    
+    return JsonResponse(data, status=status) #json.dumps content_type='application/json'
+    
