842af7e2b94235222a54422a88fafed6a84d18f8
liyahshaikh <liyahshaikh965@gmail.com>
2021-07-29 21:42:29 +0530

    Tweet-Create-View made

--- dogetwitter/tweets/views.py
+++ dogetwitter/tweets/views.py
@@ -1,17 +1,29 @@
+import random
 from django.http import HttpResponse, Http404,JsonResponse
 from django.shortcuts import render
 from .models import Tweet
+from .forms import TweetForm
 # Create your views here.
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>What color is a sunflower?<br>Yellllloow!</h1>")
     return render(request, "pages/home.html", context={}, status=200 )
+
+def tweet_create_view(request, *args,**kwargs):
+    form=TweetForm(request.POST or None)
+    if form.is_valid():
+        obj=form.save(commit=False)
+        obj.save()
+        form= TweetForm()
+    return render(request, "components/form.html", context={"form":form})
+    
+
 def tweet_list_view(request, *args, **kwargs):
     """
     REST API VIEW
     Consume by Javascript/java/swift/iOS/Android
     """
     queryList = Tweet.objects.all()
-    tweet_list = [{'id': tweet.id, 'content': tweet.content} for tweet in queryList]
+    tweet_list = [{'id': tweet.id, 'content': tweet.content, 'likes':random.randint(0,120)} for tweet in queryList]
     data={
         'response':tweet_list
     }
